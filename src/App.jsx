590e1277162b46c735a0ecaf0637ad28e4f26f4f
Dilshan Nayanamadhu Liyanage <48806985+dilshanliyanage1000@users.noreply.github.com>
2024-03-28 21:21:25 -0400

    Updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 import Navbar from "./components/Navbar";
 
 import 'bootstrap/dist/css/bootstrap.css';
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
 
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+        <nav className="navbar navbar-expand-lg fixed-top" style={{ backgroundColor: "#5C6BC0" }} data-bs-theme="dark">
             <div className="container-fluid">
                 <a href="#" className={`navbar-brand`} to="/">Student Application #3</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
